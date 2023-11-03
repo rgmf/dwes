540b840f3a450ff425b24cb3f0622033f2d797c4
Román Martínez <rgmf@riseup.net>
2023-11-03 11:28:44 +0100

    Unit 3: practice with milestone 1.

---
+++ unidades/03_arquitectura_web_wsgi_asgi/practica/random_bet_list/README.md
@@ -0,0 +1,110 @@
+# Antes de comenzar
+Estos son los requisitos generales que debes cumplir:
+
+- Usar las PEP-8 cuando escribas tu código en Python.
+- Los ficheros HTML no se modificarán, a menos que se diga lo contrario.
+- Si en el enunciado se indica, explícitamente, que se modifique un HTML, se hará siguiendo escrupulosamente este enunciado.
+- No dejes `print`s en tu código y asegúrate que, antes de entregar, eliminas todo código que uses a modo de depuración y prueba.
+- Todo código sospechoso de haber sido copiado/pegado, "chagpteado", etc, llevará al alumnado al procedimiento de "prueba de autenticidad".
+
+Esta práctica la vas a desarrollar por hitos. Cada hito logrado supondrá una nota máxima que se indicará en este enunciado. Se corregirá hasta el hito al que hayas llegado. Además, indicaré cuántas horas asignadas a clase tiene caca hito.
+
+# Descripción de la práctica en general
+En esta práctica desarrollarás una aplicación web con Python, siguiendo la especificación WSGI como hemos estado estudiando en clase estas últimas semanas.
+
+Esta aplicación tiene una serie de páginas que generará el programa Python a desarrollar en la que todo gira en torno a la elaboración de quinielas aleatorias de una competición de fútbol ficticia en la que participan 10 equipos.
+
+# Descripción del proyecto a completar que te proporciono
+Para el desarrollo de esta práctica te proporciono el siguiente proyecto a completar:
+
+``` shell
+random_bet_list
+├── README.md
+├── requirements.txt
+├── requirements_windows.txt
+├── src
+│   ├── models
+│   │   └── data.py
+│   └── views
+│       ├── gamedays.html
+│       ├── index.html
+│       └── randombetlist.html
+```
+
+El proyecto se llama `random_bet_list` y tienes, justo en al raíz de ese directorio, los ficheros:
+
+- `README.md` que este este mismo fichero que estás leyendo.
+- `requirements.txt`, el fichero con los requisitos si usas MacOs o GNU/Linux.
+- `requirements_windows.txt`, el fichero con los requisitos si usas Windows.
+
+Ahí verás, también, la carpeta `src` que es el paquete principal del programa donde irá todo el código que tengas que escribir. Ahí dentro puedes crear todo lo que necesites.
+
+Como ves, dentro de `src` ya te he dejado todos los HTML que vas a necesitar. También tienes en `models/data.py` una lista de diccionarios donde se describen las jornadas.
+
+# Hito 1: hasta 6 puntos - 2 horas de clase
+Para este primer hito, tienes que escribir el programa del servidor para responder a los siguientes *paths*:
+
+## `/home`
+Para servir la página `index.html` que ves dentro de la carpeta `views`. Es una página completa, sin *placeholders*, lista para servir directamente.
+
+## `/gamedays`
+Para servir la plantilla `gamedays.html` que ves dentro de la carpeta `views`. Aquí, tendrás que inyectar la lista de las jornadas que se pueden extraer de la lista `GAMEDAYS` que tienes en `models/data.py`. Al final, tendrás que elaborar, un listado HTML como este:
+
+``` html
+<ul>
+	<li>Jornada 1</li>
+	<li>Jornada 2</li>
+	<li>Jornada 3</li>
+</ul>
+```
+
+Que inyectarás en el *placeholder* **{gamedays_list}** que puedes ver en dicha plantilla.
+
+Esos números de jornadas se pueden extraer de la lista de diccionarios `GAMEDAYS`. Analízalo.
+
+## `/randombetlist`
+Para servir la página `randombetlist.html` que ves dentro de la carpeta `views`. Aquí, tendrás que inyectar la información de los partidos de la última jornada con información de los resultados que generarás al azar. ¿De dónde sale esta información? De la lista de diccionarios `GAMEDAYS`. Analízalo.
+
+Si te fijas en la plantilla `randombetlist.html`, dentro del `main`, hay un `section`, dentro del cual hay un `article` con este contenido:
+
+``` html
+<h2>Quiniela aleatoria generada para la jornada {gameday_number}</h2>
+
+<table>
+  <tr>
+    <th>Local</th>
+    <th>Visitante</th>
+    <th>Pronóstico</th>
+  </tr>
+  <tr>
+    {game1}
+  </tr>
+  <tr>
+    {game2}
+  </tr>
+  <tr>
+    {game3}
+  </tr>
+  <tr>
+    {game4}
+  </tr>
+  <tr>
+    {game5}
+  </tr>
+</table>
+```
+
+En el, como puedes ver, tienes que inyectar bastantes cosas en estos *placeholders*:
+
+- `gameday_number`: el número de la jornada (en este caso la última) que sacarás de `GAMEDAYS`.
+- `game1`, `game2`, `game3`, `game4` y `game5`: en cada uno de estos *placeholders* tienes que inyectar la información del partido en cuestión. Lo que, al final inyectarás aquí se parecerá a esto:
+
+``` html
+<td>Atlético de Madrid</td><td>Osasuna</td><td>X</td>
+```
+
+El nombre de los equipos lo sacas de la jornada en concreto de `GAMEDAYS` y la **X** se generará al azar. Donde pone **X** puede aparecer una de estas opciones:
+
+- **1**: gana el equipo local.
+- **2**: gana el equipo visitante.
+- **X**: empatan.
---
+++ unidades/03_arquitectura_web_wsgi_asgi/practica/random_bet_list/src/models/data.py
@@ -0,0 +1,182 @@
+GAMEDAYS = [
+    {
+        "gameday": 1,
+        "games": [
+            {"team1": "Atlético de Madrid", "team2": "Osasuna"},
+            {"team1": "Villarreal", "team2": "Valencia"},
+            {"team1": "Athlétic de Bilbao", "team2": "FC Barcelona"},
+            {"team1": "Real Madrid", "team2": "Sevilla FC"},
+            {"team1": "Real Sociedad", "team2": "Celta de Vigo"}
+        ]
+    },
+    {
+        "gameday": 2,
+        "games": [
+            {"team1": "Osasuna", "team2": "Villarreal"},
+            {"team1": "Valencia", "team2": "Athlétic de Bilbao"},
+            {"team1": "FC Barcelona", "team2": "Real Madrid"},
+            {"team1": "Sevilla FC", "team2": "Real Sociedad"},
+            {"team1": "Celta de Vigo", "team2": "Atlético de Madrid"}
+        ]
+    },
+    {
+        "gameday": 3,
+        "games": [
+            {"team1": "Atlético de Madrid", "team2": "Villarreal"},
+            {"team1": "Osasuna", "team2": "Valencia"},
+            {"team1": "Athlétic de Bilbao", "team2": "Sevilla FC"},
+            {"team1": "Real Madrid", "team2": "Celta de Vigo"},
+            {"team1": "FC Barcelona", "team2": "Real Sociedad"}
+        ]
+    },
+    {
+        "gameday": 4,
+        "games": [
+            {"team1": "Villarreal", "team2": "FC Barcelona"},
+            {"team1": "Valencia", "team2": "Sevilla FC"},
+            {"team1": "Athlétic de Bilbao", "team2": "Celta de Vigo"},
+            {"team1": "Real Madrid", "team2": "Real Sociedad"},
+            {"team1": "Osasuna", "team2": "Atlético de Madrid"}
+        ]
+    },
+    {
+        "gameday": 5,
+        "games": [
+            {"team1": "Atlético de Madrid", "team2": "Valencia"},
+            {"team1": "Villarreal", "team2": "Sevilla FC"},
+            {"team1": "Osasuna", "team2": "Celta de Vigo"},
+            {"team1": "FC Barcelona", "team2": "Real Sociedad"},
+            {"team1": "Athlétic de Bilbao", "team2": "Real Madrid"}
+        ]
+    },
+    {
+        "gameday": 6,
+        "games": [
+            {"team1": "Valencia", "team2": "Real Madrid"},
+            {"team1": "Sevilla FC", "team2": "Celta de Vigo"},
+            {"team1": "Villarreal", "team2": "Real Sociedad"},
+            {"team1": "FC Barcelona", "team2": "Atlético de Madrid"},
+            {"team1": "Osasuna", "team2": "Athlétic de Bilbao"}
+        ]
+    },
+    {
+        "gameday": 7,
+        "games": [
+            {"team1": "Atlético de Madrid", "team2": "Valencia"},
+            {"team1": "Villarreal", "team2": "Real Madrid"},
+            {"team1": "Osasuna", "team2": "Celta de Vigo"},
+            {"team1": "FC Barcelona", "team2": "Sevilla FC"},
+            {"team1": "Athlétic de Bilbao", "team2": "Real Sociedad"}
+        ]
+    },
+    {
+        "gameday": 8,
+        "games": [
+            {"team1": "Valencia", "team2": "Sevilla FC"},
+            {"team1": "Real Madrid", "team2": "Celta de Vigo"},
+            {"team1": "Villarreal", "team2": "Atlético de Madrid"},
+            {"team1": "FC Barcelona", "team2": "Real Sociedad"},
+            {"team1": "Osasuna", "team2": "Athlétic de Bilbao"}
+        ]
+    },
+    {
+        "gameday": 9,
+        "games": [
+            {"team1": "Atlético de Madrid", "team2": "Real Sociedad"},
+            {"team1": "Villarreal", "team2": "Celta de Vigo"},
+            {"team1": "Osasuna", "team2": "Real Madrid"},
+            {"team1": "FC Barcelona", "team2": "Athlétic de Bilbao"},
+            {"team1": "Valencia", "team2": "Sevilla FC"}
+        ]
+    },
+    {
+        "gameday": 10,
+        "games": [
+            {"team1": "Osasuna", "team2": "Atletico de Madrid"},
+            {"team1": "Valencia", "team2": "Villarreal"},
+            {"team1": "FC Barcelona", "team2": "Athletic de Bilbao"},
+            {"team1": "Sevilla FC", "team2": "Real Madrid"},
+            {"team1": "Celta de Vigo", "team2": "Real Sociedad"}
+        ]
+    },
+    {
+        "gameday": 11,
+        "games": [
+            {"team1": "Villarreal", "team2": "Atletico de Madrid"},
+            {"team1": "Athletic de Bilbao", "team2": "Osasuna"},
+            {"team1": "Real Madrid", "team2": "FC Barcelona"},
+            {"team1": "Real Sociedad", "team2": "Sevilla FC"},
+            {"team1": "Atletico de Madrid", "team2": "Celta de Vigo"}
+        ]
+    },
+    {
+        "gameday": 12,
+        "games": [
+            {"team1": "Villarreal", "team2": "Atletico de Madrid"},
+            {"team1": "Valencia", "team2": "Osasuna"},
+            {"team1": "Sevilla FC", "team2": "Athletic de Bilbao"},
+            {"team1": "Celta de Vigo", "team2": "Real Madrid"},
+            {"team1": "Real Sociedad", "team2": "FC Barcelona"}
+        ]
+    },
+    {
+        "gameday": 13,
+        "games": [
+            {"team1": "FC Barcelona", "team2": "Villarreal"},
+            {"team1": "Sevilla FC", "team2": "Valencia"},
+            {"team1": "Celta de Vigo", "team2": "Athletic de Bilbao"},
+            {"team1": "Real Sociedad", "team2": "Real Madrid"},
+            {"team1": "Atletico de Madrid", "team2": "Osasuna"}
+        ]
+    },
+    {
+        "gameday": 14,
+        "games": [
+            {"team1": "Valencia", "team2": "Atletico de Madrid"},
+            {"team1": "Sevilla FC", "team2": "Villarreal"},
+            {"team1": "Celta de Vigo", "team2": "Osasuna"},
+            {"team1": "Real Sociedad", "team2": "FC Barcelona"},
+            {"team1": "Real Madrid", "team2": "Athletic de Bilbao"}
+        ]
+    },
+    {
+        "gameday": 15,
+        "games": [
+            {"team1": "Real Madrid", "team2": "Valencia"},
+            {"team1": "Celta de Vigo", "team2": "Sevilla FC"},
+            {"team1": "Real Sociedad", "team2": "Villarreal"},
+            {"team1": "Atletico de Madrid", "team2": "FC Barcelona"},
+            {"team1": "Athletic de Bilbao", "team2": "Osasuna"}
+        ]
+    },
+    {
+        "gameday": 16,
+        "games": [
+            {"team1": "Valencia", "team2": "Atletico de Madrid"},
+            {"team1": "Real Madrid", "team2": "Villarreal"},
+            {"team1": "Celta de Vigo", "team2": "Osasuna"},
+            {"team1": "FC Barcelona", "team2": "Sevilla FC"},
+            {"team1": "Athletic de Bilbao", "team2": "Real Sociedad"}
+        ]
+    },
+    {
+        "gameday": 17,
+        "games": [
+            {"team1": "Sevilla FC", "team2": "Valencia"},
+            {"team1": "Celta de Vigo", "team2": "Real Madrid"},
+            {"team1": "Atletico de Madrid", "team2": "Villarreal"},
+            {"team1": "FC Barcelona", "team2": "Real Sociedad"},
+            {"team1": "Osasuna", "team2": "Athletic de Bilbao"}
+        ]
+    },
+    {
+        "gameday": 18,
+        "games": [
+            {"team1": "Real Sociedad", "team2": "Atletico de Madrid"},
+            {"team1": "Celta de Vigo", "team2": "Villarreal"},
+            {"team1": "Real Madrid", "team2": "Osasuna"},
+            {"team1": "Athletic de Bilbao", "team2": "FC Barcelona"},
+            {"team1": "Sevilla FC", "team2": "Valencia"}
+        ]
+    }
+]
