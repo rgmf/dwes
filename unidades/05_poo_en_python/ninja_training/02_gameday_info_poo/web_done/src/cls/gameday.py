6cae143979163704645c3a910dba869596b6b3bc
Román Martínez <rgmf@riseup.net>
2023-11-21 15:09:09 +0100

    Unit 5: move nina trainings.

--- unidades/05_poo_en_python/ninja_training/02_gameday_info_poo/web_done/src/cls/gameday.py
+++
@@ -1,8 +0,0 @@
-from src.cls.game import Game
-
-
-class Gameday:
-    def __init__(self, gameday: int, played: bool, games: list[Game]) -> None:
-        self.gameday: int = gameday
-        self.played: bool = played
-        self.games: list[Game] = games
