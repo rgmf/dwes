7212f87b041abb242a41ab21c73ec08fa3b39054
Román Martínez <rgmf@riseup.net>
2024-01-11 19:46:35 +0100

    Unit 7: ninja trainings: improved urls.

--- unidades/07_django/ninja_trainings/05_django_templates_herencia/done/dwes/memora/urls.py
+++ unidades/07_django/ninja_trainings/05_django_templates_herencia/done/dwes/memora/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("notes/", views.notes, name="notes"),
-    path("<int:note_id>/", views.detail, name="detail")
+    path("notes/<int:note_id>/", views.detail, name="detail")
 ]
--- unidades/07_django/ninja_trainings/06_django_forms/done/dwes/memora/urls.py
+++ unidades/07_django/ninja_trainings/06_django_forms/done/dwes/memora/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("notes/", views.notes, name="notes"),
-    path("<int:note_id>/", views.detail, name="detail"),
+    path("notes/<int:note_id>/", views.detail, name="detail"),
     path("notes/create/", views.create_note, name="create_note")
 ]
